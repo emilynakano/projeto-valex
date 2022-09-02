140ecf2bd350db744a480af0bd30b831ffc9369e
emily <nakanodepaula@gmail.com>
2022-09-02 08:48:39 -0300

    feat: employee validation

--- src/services/cardService.ts
+++ src/services/cardService.ts
@@ -1,17 +1,18 @@
 import { TransactionTypes } from "../repositories/cardRepository";
 import { findByApiKey } from "../repositories/companyRepository";
-import { unauthorizedError } from "../middlewares/errorHandlingMiddleware";
+import { findById } from "../repositories/employeeRepository";
+import { notFoundError, unauthorizedError } from "../middlewares/errorHandlingMiddleware";
 
 export async function createCard (
     employeeId:number, 
     type:TransactionTypes, 
     apiKey: any
 ) {
-    
-    const apiKeyValidation = await findByApiKey(apiKey);
-   
-    if(!apiKeyValidation) {
-        throw unauthorizedError('apiKey')
-    }
 
+    const apiKeyValidation = await findByApiKey(apiKey);
+    if(!apiKeyValidation) throw unauthorizedError('apiKey')
+    
+    const employee = await findById(employeeId);
+    if(!employee) throw notFoundError('employee');
+    
 }
